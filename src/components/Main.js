21eb7414868c6ab4adcec8130cdc1bc0814647c9
Sara Le Lait <sjlelait@gmail.com>
2024-01-03 18:57:03 -0700

    wip: ReadIndex displaying entries

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import Read from '../pages/Read';
@@ -5,11 +6,27 @@ import ReadIndex from '../pages/ReadIndex';
 import AlsoKnownAs from '../pages/AlsoKnownAs';
 
 const Main = (props) => {
+    const [entry, setEntry] = useState(null);
+    const API_URL = 'https://deckled-edge-backend-988afb1cc431.herokuapp.com/';
+
+    const getEntry = async () => {
+        try {
+            const response = await fetch(API_URL);
+            const data = await response.json();
+            setEntry(data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    useEffect(() => {
+        getEntry();
+    }, []);
+
     return (
         <main>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/read" element={<ReadIndex />} />
+                <Route path="/read" element={<ReadIndex entry={entry} />} />
                 <Route path="/read/:id" element={<Read />} />
                 <Route path="/aka" element={<AlsoKnownAs />} />
             </Routes>
--- src/pages/ReadIndex.js
+++ src/pages/ReadIndex.js
@@ -1,5 +1,22 @@
+import { Link } from 'react-router-dom';
+
 const ReadIndex = (props) => {
-    return <h1>Read Index (past Entries)</h1>;
+    const loaded = () => {
+        return props.entry.map((entry) => (
+            <div key={entry._id} className="entry">
+                <Link to={`/read/${entry._id}`}>
+                    <h2>{entry.title}</h2>
+                </Link>
+                <p>{entry.text}</p>
+            </div>
+        ));
+    };
+
+    const loading = () => {
+        return <h1>Loading...</h1>;
+    };
+
+    return props.entry ? loaded() : loading();
 }
 
 export default ReadIndex;
