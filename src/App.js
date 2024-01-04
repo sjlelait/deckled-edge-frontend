70eca2256e48d0687b96f2aefc82eb9eed4bdb5b
Sara Le Lait <sjlelait@gmail.com>
2024-01-03 17:49:03 -0700

    wip: adds routes to Main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Header from './components/Header';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Deckled Edge</h1>
-      </header>
+      <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Read from '../pages/Read';
+import ReadIndex from '../pages/ReadIndex';
+import AlsoKnownAs from '../pages/AlsoKnownAs';
+
+function Main(props) {
+    return (
+        <main>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/read" element={<ReadIndex />} />
+                <Route path="/read/:id" element={<Read />} />
+                <Route path="/aka" element={<AlsoKnownAs />} />
+            </Routes>
+        </main>
+    );
+}
+
+export default Main;
