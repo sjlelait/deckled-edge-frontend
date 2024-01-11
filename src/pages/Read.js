c3803ad4e93737fb77f513349d90cf71567ac577
Sara Le Lait <sjlelait@gmail.com>
2024-01-10 17:35:16 -0700

    wip: creates working read/_id page

--- src/pages/Read.js
+++ src/pages/Read.js
@@ -1,5 +1,27 @@
+import { useParams } from "react-router-dom";
+
 const Read = (props) => {
-    return <h1>Read</h1>;
+    const { id } = useParams();
+    const entry = props.entry;
+    const oneEntry = entry ? entry.find((e) => e._id === id) : null;
+
+    const loaded = () => {
+        return (
+            <>
+                <h1>{oneEntry.title}</h1>
+                <p>{oneEntry.text}</p>
+            </>
+        );
+    };
+    const loading = () => {
+        return <h1>Loading...</h1>
+    };
+
+    return (
+        <div className="oneEntry">
+            {oneEntry ? loaded() : loading()}
+        </div>
+    );
 }
 
 export default Read;
