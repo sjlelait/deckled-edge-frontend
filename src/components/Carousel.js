4fd4f25cf82eb991931a8b36d706fc0cff07d377
Sara Le Lait <sjlelait@gmail.com>
2024-02-11 14:04:45 -0700

    wip: edits home styling for simplicity

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -10,19 +10,20 @@ const Carousel = (props) => {
     const loaded = () => {
         return (
             <>
-                <TECarousel showControls showIndicators ride="carousel">
+                <TECarousel showControls showIndicators ride="carousel" className="bg-white">
                     <div className="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
+                        <h1 className="text-2xl mt-4">Featured Writing</h1>
                         {filteredEntries.slice(0, 3).map((entry, index) => (
                             <TECarouselItem
                                 key={entry._id}
                                 itemID={index + 1}
                                 className={`relative float-left -mr-[100%] ${index === 0 ? 'block' : 'hidden'
-                                    } w-full transition-opacity duration-[600ms] motion-reduce:transition-none`}
+                                    } w-full transition-opacity duration-[1000ms] motion-reduce:transition-none`}
                             >
                                 <div key={entry._id} className="flex-shrink-0 w-full">
-                                    <div className="flex-row justify-center h-full bg-white p-6">
+                                    <div className="flex-row justify-center h-full bg-white p-6 m-6">
                                         <Link to={`/read/${entry._id}`}>
-                                            <h2 className="text-lg">{entry.title}</h2>
+                                            <h2 className="text-xl mb-4">{entry.title}</h2>
                                         </Link>
                                         <p style={{ whiteSpace: "pre-line" }} className="text-gray-500">{entry.text}</p>
                                     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,29 +4,29 @@ import Carousel from '../components/Carousel';
 
 const Home = (props) => {
     return (
-        <div>
-            <div className="mx-auto mt-20 mb-10 flex items-center justify-between">
+        <>
+            <div className="mx-auto pt-20 pb-10 flex items-center justify-between bg-background">
                 <Link
                     to="/write"
-                    className="py-2 px-4 ml-10 bg-white font-semibold rounded-lg shadow-lg hover:text-white hover:bg-mainPurple"
+                    className="py-2 px-4 ml-10 bg-white font-semibold rounded-lg shadow-lg hover:bg-mainPurple"
                 >
                     Write
                 </Link>
                 <Link
                     to="/aka"
-                    className="py-2 px-4 bg-white font-semibold rounded-lg shadow-lg hover:text-white hover:bg-mainPurple"
+                    className="py-2 px-4 bg-white font-semibold rounded-lg shadow-lg hover:bg-mainPurple"
                 >
                     Also Known As
                 </Link>
                 <Link
                     to="/read"
-                    className="py-2 px-4 mr-10 bg-white font-semibold rounded-lg shadow-lg hover:text-white hover:bg-mainPurple"
+                    className="py-2 px-4 mr-10 bg-white font-semibold rounded-lg shadow-lg hover:bg-mainPurple"
                 >
                     Read
                 </Link>
             </div>
             {props.entry && <Carousel entry={props.entry} />}
-        </div>
+        </>
     )
 }
 
