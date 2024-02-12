b9a4238e8bcd008475e86e17c4e56c0338b5837d
Sara Le Lait <sjlelait@gmail.com>
2024-02-11 17:36:57 -0700

    wip: adds footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
       <Main />
-    </div>
+      <Footer />
+    </div >
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
 const Footer = (props) => {
-    <>
-        <h1>Footer</h1>
-    </>
+    return (
+        <footer className="fixed bottom-0 left-0 right-0 w-full text-center justify-between bg-mainPurple">
+            <div className="container mx-auto px-4 lg:px-8">
+                <div className="flex flex-col items-center justify-center p-2 lg:justify-between">
+                    <div className="text-center">
+                        <p className="text-sm">© Deckled Edge {new Date().getFullYear()}</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
 }
 export default Footer;
