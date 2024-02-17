715c8678296b8855162183a80d9693f94429b762
Sara Le Lait <sjlelait@gmail.com>
2024-02-17 15:21:10 -0700

    wip: initializes firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAM1IZiw_3t3TccIOIVPlHPSkx3ur0ZRsk",
+    authDomain: "deckled-edge.firebaseapp.com",
+    projectId: "deckled-edge",
+    storageBucket: "deckled-edge.appspot.com",
+    messagingSenderId: "506107436703",
+    appId: "1:506107436703:web:aff3a2f5bb12ccf2cfccaa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
