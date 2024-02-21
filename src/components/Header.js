e89d56ab06e9b4954a9cf8793ad4dd40494f0b54
Sara Le Lait <sjlelait@gmail.com>
2024-02-20 17:31:14 -0700

    wip: adds image to user

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,8 +12,12 @@ const Header = (props) => {
             </div>
             <nav className="ml-auto flex max-w-7xl items-center justify-between h-16 p-6 lg:px-6">
                 {props.user ?
-
                     <>
+                        <div className="pr-5">
+                            <img className="rounded-full w-10 h-10"
+                                src={props.user.photoURL}
+                                alt={props.user.displayName} />
+                        </div>
                         <div className="inline-block items-center hover:shadow-lg hover:rounded-lg p-2">
                             <Link to="/read">
                                 <div>Read</div>
@@ -30,7 +34,6 @@ const Header = (props) => {
                         <div className="inline-block items-center hover:shadow-lg hover:rounded-lg p-2">
                             <button onClick={logout}>Logout</button>
                         </div>
-
                     </>
                     :
                     <>
