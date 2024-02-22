9ffb7c1ef22953a015d2198f7fcfecee2ca2b463
Sara Le Lait <sjlelait@gmail.com>
2024-02-21 17:16:58 -0700

    wip: adds conditional user render on home buttons

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,24 +6,37 @@ const Home = (props) => {
     return (
         <>
             <div className="mx-auto pt-12 pb-12 flex items-center justify-between bg-background">
-                <Link
-                    to="/write"
-                    className="py-2 px-4 ml-10 bg-white font-semibold rounded-lg shadow-lg hover:bg-mainPurple"
-                >
-                    Write
-                </Link>
-                <Link
-                    to="/read"
-                    className="py-2 px-4 bg-white font-semibold rounded-lg shadow-lg hover:bg-mainPurple"
-                >
-                    Read
-                </Link>
-                <Link
-                    to="/aka"
-                    className="py-2 px-4 mr-6 bg-white font-semibold rounded-lg shadow-lg hover:bg-mainPurple"
-                >
-                    Also Known As
-                </Link>
+                {props.user ?
+                    <>
+                        <Link
+                            to="/write"
+                            className="py-2 px-4 ml-10 bg-white font-semibold rounded-lg shadow-lg hover:bg-mainPurple"
+                        >
+                            Write
+                        </Link>
+                        <Link
+                            to="/read"
+                            className="py-2 px-4 bg-white font-semibold rounded-lg shadow-lg hover:bg-mainPurple"
+                        >
+                            Read
+                        </Link>
+                        <Link
+                            to="/aka"
+                            className="py-2 px-4 mr-6 bg-white font-semibold rounded-lg shadow-lg hover:bg-mainPurple"
+                        >
+                            Also Known As
+                        </Link>
+                    </>
+                    :
+                    <>
+                        <Link
+                            to="/aka"
+                            className="py-2 px-4 ml-5 bg-white font-semibold rounded-lg shadow-lg hover:bg-mainPurple"
+                        >
+                            Also Known As
+                        </Link>
+                    </>
+                }
             </div>
             {props.entry && <Carousel entry={props.entry} />}
         </>
